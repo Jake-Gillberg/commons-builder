1b5f9734d22e1d9ab47ce5814c006a59a2114875
Jake Gillberg <jake.gillberg@gmail.com>
2019-07-07 04:56:35 +0000

    small fixes to *broken* test file as seen in a github issue comment

--- test/index.test.ts
+++ test/index.test.ts
@@ -23,21 +23,21 @@ describe('My Probot app', () => {
     app.app = () => 'test'
   })
 
-  test('creates a comment when an issue is opened', async (done) => {
+  test('creates a passing check', async () => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
       .reply(200, { token: 'test' })
 
-    // Test that a comment is posted
+    // Test that a commented is posted
     nock('https://api.github.com')
       .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
+        expect(body).toMatchObject(issueCreatedBody)
         return true
       })
       .reply(200)
 
-    // Receive a webhook event
+    // Recieve a webhook event
     await probot.receive({ name: 'issues', payload })
   })
 })
