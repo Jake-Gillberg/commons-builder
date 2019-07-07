7f355bce1274fdb82213fc05f17f73285c9ebd2c
Jake Gillberg <jake.gillberg@gmail.com>
2019-07-07 16:56:00 +0000

    lint tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -17,7 +17,7 @@ describe('My Probot app', () => {
   beforeEach(() => {
     probot = new Probot({ id: 123, githubToken: 'test' })
     // Load our app into probot
-    const app = probot.load(myProbotApp)
+    probot.load(myProbotApp)
   })
 
   test('creates a passing check', async () => {
